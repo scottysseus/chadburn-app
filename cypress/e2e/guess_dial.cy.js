af3d8cd58a545f01015b63d02bb7848c490b5e1e
scottysseus <sweidenkopf@gmail.com>
2023-01-09 12:59:23 -0500

    Scottysseus/cypress (#16)
    * tryin' out cypress
    
    * adding functions for determining the angle of the guess dial
    
    * added working test - guess dial can be rotated
    
    * remove unused data-cy tag for now
    
    * updated README with Cypress instructions
    
    * remove console logs
    
    * renaming helper function
    
    Co-authored-by: Scott Weidenkopf <sweidenk@google.com>

---
+++ cypress/e2e/guess_dial.cy.js
@@ -0,0 +1,31 @@
+/// <reference types="Cypress" />
+
+import { PlayerView } from "../lib/PlayerView";
+
+describe("guess dial", () => {
+  it("can be rotated", () => {
+    cy.visit("/");
+
+    cy.get('[data-cy="landing_btn_new_game"]').click();
+
+    PlayerView.get_guess_angle().should("equal", 0);
+
+    PlayerView.get_guess_dial().then(($guessDial) => {
+      const offset = $guessDial.offset();
+
+      const downX = $guessDial.width() / 2 + offset.left;
+      const downY = $guessDial.height() / 4 + offset.top;
+
+      cy.wrap($guessDial)
+        .trigger("mousedown", { clientX: downX, clientY: downY, which: 1 })
+        .trigger("mousemove", {
+          clientX: downX - 10,
+          clientY: downY,
+          which: 1,
+        })
+        .trigger("mouseup");
+    });
+
+    PlayerView.get_guess_angle().should("not.equal", 0);
+  });
+});
---
+++ cypress/lib/PlayerView.js
@@ -0,0 +1,54 @@
+// matches e.g. rotate(-90deg) and captures the numeric value (-90 in this case)
+const rotateDegreeRegex = /^rotate\(([-]?[0-9]+)deg\)$/;
+const matrixRegx = /^matrix\(.*\)$/;
+
+/**
+ * Copied from source :)
+ * Normalizes the degrees to within -90° and 90°.
+ * @param degrees degrees to normalize
+ * @returns the degrees normalized within -90° and 90°.
+ */
+function normalizeDegrees(degrees) {
+  degrees %= 360;
+  if (degrees < 0) {
+    degrees += 360;
+  }
+  if (degrees > 180) {
+    degrees -= 360;
+  }
+
+  return degrees;
+}
+
+function getRotationDegreesFromCssMatrix(cssMatrix) {
+  let angle = 0;
+  if (cssMatrix !== "none") {
+    const values = cssMatrix.split("(")[1].split(")")[0].split(",");
+    const a = values[0];
+    const b = values[1];
+    angle = Math.round(Math.atan2(b, a) * (180 / Math.PI));
+  }
+  return normalizeDegrees(angle < 0 ? angle + 360 : angle);
+}
+
+export const PlayerView = {
+  get_guess_dial() {
+    return cy.get('img[src="assets/guess.svg"]');
+  },
+  get_guess_angle() {
+    return PlayerView.get_guess_dial().then(($guessDial) => {
+      const transform = $guessDial.css("transform");
+      let angle = NaN;
+
+      if (rotateDegreeRegex.test(transform)) {
+        const match = rotateDegreeRegex.exec(transform);
+        angle = match[1];
+      } else if (matrixRegx.test(transform)) {
+        const match = matrixRegx.exec(transform);
+        angle = getRotationDegreesFromCssMatrix(match[0]);
+      }
+
+      return cy.wrap(angle);
+    });
+  },
+};
