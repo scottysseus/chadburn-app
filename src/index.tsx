7e78ab0441bb4e34419a9aa176dc27e888f50c09
scottysseus <sweidenkopf@gmail.com>
2023-01-15 16:27:09 -0500

    starting Landing page styling

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,8 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import { App } from "./App";
 
+import "./index.module.css";
+
 window.onload = function () {
   const container = document.getElementById("root");
   const root = createRoot(container!);
--- src/scenes/Landing.tsx
+++ src/scenes/Landing.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useNavigate } from "react-router";
 import { v4 as uuidv4 } from "uuid";
+import styles from "./Landding.module.css";
 
 export const Landing = () => {
   const navigate = useNavigate();
@@ -10,10 +11,11 @@ export const Landing = () => {
   };
 
   return (
-    <>
+    <div className={styles.landingMenuContainer}>
+      <h1>Chadburn</h1>
       <button data-cy="landing_btn_new_game" onClick={onClick}>
-        New Game
+        NEW GAME
       </button>
-    </>
+    </div>
   );
 };
