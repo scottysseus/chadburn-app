0eb017ab2084e770825e9ed582d64b6c126c03f5
scottysseus <sweidenkopf@gmail.com>
2023-01-29 12:15:31 -0500

    added comments

--- src/scenes/game/GuessForm.tsx
+++ src/scenes/game/GuessForm.tsx
@@ -20,6 +20,13 @@ export const GuessForm = ({
   const onInputChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
     let newGuess = event.target.value;
 
+    /**
+     * Only propagate the update upward if it is a valid number.
+     * Otherwise, keep the update local to allow the user to fix it.
+     *
+     * e.g. if they type '-', don't propagate NaN upward. Let the user
+     * finish typing '-10'.
+     */
     if (!isGuessInvalid(newGuess)) {
       const newGuessAsNumber = Number(newGuess);
       if (newGuessAsNumber > 90) newGuess = String(90);
@@ -34,14 +41,17 @@ export const GuessForm = ({
     return isNaN(Number(guess)) || guess === "";
   };
 
+  /**
+   * Use input with type="text" because built-in number inputs suck.
+   * Their validation happens too early, so they don't allow the user to
+   * 'finish their thought'.
+   *
+   * The main issue is, users cannot fully clear (using backspace) the text box, making entering negative
+   * numbers difficult.
+   */
   return (
     <>
-      <input
-        inputMode="decimal"
-        type="text"
-        value={localGuess}
-        onChange={onInputChange}
-      />
+      <input type="text" value={localGuess} onChange={onInputChange} />
       <button onClick={onGuessSubmitted} disabled={isGuessInvalid(localGuess)}>
         Submit
       </button>
