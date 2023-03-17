d310de596cc3b1c71159d1f0d0809b17ff8e2c3e
scottysseus <sweidenkopf@gmail.com>
2023-03-17 19:14:34 -0400

    adding instructions for each of the forms

--- src/scenes/game/RebuttalForm.tsx
+++ src/scenes/game/RebuttalForm.tsx
@@ -17,23 +17,28 @@ export const RebuttalForm = ({
   onRebuttalSubmit,
 }: RebuttalViewProps) => {
   return (
-    <>
-      <Toggle
-        left="Left"
-        right="Right"
-        isLeft={rebuttal === Rebuttals.LEFT}
-        onToggle={(isLeft) =>
-          onRebuttalUpdate(isLeft ? Rebuttals.LEFT : Rebuttals.RIGHT)
-        }
-      />
+    <div>
+      <p className="formDescription">
+        Is the hint to the left or the right of the other team&apos;s guess?
+      </p>
+      <div className="formInput">
+        <Toggle
+          left="Left"
+          right="Right"
+          isLeft={rebuttal === Rebuttals.LEFT}
+          onToggle={(isLeft) =>
+            onRebuttalUpdate(isLeft ? Rebuttals.LEFT : Rebuttals.RIGHT)
+          }
+        />
 
-      <button
-        data-cy="game_btn_submit_rebuttal"
-        onClick={onRebuttalSubmit}
-        style={{ marginLeft: "20px" }}
-      >
-        Submit
-      </button>
-    </>
+        <button
+          data-cy="game_btn_submit_rebuttal"
+          onClick={onRebuttalSubmit}
+          style={{ marginLeft: "20px" }}
+        >
+          Submit
+        </button>
+      </div>
+    </div>
   );
 };
