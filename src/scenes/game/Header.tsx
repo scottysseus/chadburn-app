5563320a71e3d96b24300fd5244aef70f7f95335
scottysseus <sweidenkopf@gmail.com>
2023-04-04 09:14:42 -0400

    coloring the team text with their respective colors (#47)
    * coloring the team text with their respective colors
    
    * adding team names + colors to rebuttal prompt

--- src/scenes/game/Header.tsx
+++ src/scenes/game/Header.tsx
@@ -12,12 +12,17 @@ export const Header = ({ score, teamInTurn }: HeaderProps) => {
   return (
     <div className="header">
       <p>
-        Blue: <span data-cy="game_score_blue">{score.get("blue")}</span> Red:{" "}
-        <span data-cy="game_score_red">{score.get("red")}</span>
+        <span style={{ color: "blue" }}>
+          Blue: <span data-cy="game_score_blue">{score.get("blue")}</span>
+        </span>{" "}
+        <span style={{ color: "red" }}>
+          Red: <span data-cy="game_score_red">{score.get("red")}</span>
+        </span>
       </p>
-
       {!isGameOver(score) && (
-        <p className="turnTracker">{teamInTurn} team&apos;s turn!</p>
+        <p className="turnTracker" style={{ color: teamInTurn }}>
+          {teamInTurn} team&apos;s turn!
+        </p>
       )}
     </div>
   );
