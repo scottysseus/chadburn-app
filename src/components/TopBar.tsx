09f8b987e7eb32fec65c9f78e1b7600fcb992fd9
AWeidenkopf <oliveira.anacalado@gmail.com>
2023-04-26 11:03:06 -0400

    initiate copy link functionality creates the button; adds the functionality to it;

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,6 +1,10 @@
 import React from "react";
 import "./TopBar.module.scss";
 
+const onCopyLinkClick = () => {
+  navigator.clipboard.writeText(window.location.href);
+};
+
 export const TopBar = () => {
   return (
     <div className="topBar">
@@ -9,6 +13,7 @@ export const TopBar = () => {
           CHADBURN
         </a>
       </h1>
+      <a onClick={onCopyLinkClick}>Copy Link</a>
     </div>
   );
 };
