5e06fe3bcb0d4ad1dd632e8b0601241bdff2a302
AWeidenkopf <oliveira.anacalado@gmail.com>
2023-04-27 20:01:09 -0400

    position the button to the left

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,19 +1,27 @@
 import React from "react";
 import "./TopBar.module.scss";
 
+/**
+ * TODO show copy button only in the game page ??
+ */
+
 const onCopyLinkClick = () => {
   navigator.clipboard.writeText(window.location.href);
 };
 
 export const TopBar = () => {
   return (
-    <div className="topBar">
-      <h1>
-        <a href="/" className="typewriter">
-          CHADBURN
-        </a>
-      </h1>
-      <a onClick={onCopyLinkClick}>Copy Link</a>
-    </div>
+    <>
+      <div className="topBar">
+        <h1>
+          <a href="/" className="typewriter">
+            CHADBURN
+          </a>
+        </h1>
+      </div>
+      <a onClick={onCopyLinkClick} className="clipboard">
+        Copy Link
+      </a>
+    </>
   );
 };
